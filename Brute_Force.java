8483451dacb32901cc507b69260ea4f20d321472
justanotherinternetguy <hendryxu1@gmail.com>
2023-05-01 12:57:04 -0400

    Commit

--- Brute_Force.java
+++ Brute_Force.java
@@ -1,10 +1,76 @@
 import java.io.*;
 import java.util.*;
+
 public class Brute_Force {
     public static void main(String[] args) {
-        // Home, Aldo, Butch, Cassidy
+        Map<Integer, List<Edge>> adjacencyList = new HashMap<>();
 
+        int numVertices = 2;
+        for (int i = 1; i <= numVertices; i++) {
+            adjacencyList.put(i, new ArrayList<>());
+        }
 
+        adjacencyList.get(1).add(new Edge(2, 3));
 
+        for (int vertex : adjacencyList.keySet()) {
+            System.out.print(vertex + ": ");
+            for (Edge edge : adjacencyList.get(vertex)) {
+                System.out.print("(" + edge.destination + ", " + edge.weight + ") ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+public static class Edge {
+    int destination;
+    int weight;
+
+    public Edge(int destination, int weight) {
+        this.destination = destination;
+        this.weight = weight;
     }
 }
+
+public class WeightedGraph {
+    private Map<Integer, List<Edge>> adjacencyList;
+
+    public WeightedGraph() {
+        adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(int vertex) {
+        adjacencyList.put(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(int source, int destination, int weight) {
+        Edge edge = new Edge(destination, weight);
+        adjacencyList.get(source).add(edge);
+    }
+
+    public List<Edge> getEdges(int vertex) {
+        return adjacencyList.get(vertex);
+    }
+
+    public int getWeight(int source, int destination) {
+        List<Edge> edges = adjacencyList.get(source);
+        for (Edge edge : edges) {
+            if (edge.destination == destination) {
+                return edge.weight;
+            }
+        }
+        return -1;
+    }
+
+    private static class Edge {
+        int destination;
+        int weight;
+
+        public Edge(int destination, int weight) {
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+}
+
