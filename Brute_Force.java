39c77a4b5229052abba0eabd119ac2f911f21f2e
justanotherinternetguy <hendryxu1@gmail.com>
2023-05-01 12:17:18 -0400

    Commit

--- Brute_Force.java
+++ Brute_Force.java
@@ -2,8 +2,18 @@ import java.io.*;
 import java.util.*;
 public class Brute_Force {
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>;
         // Home, Aldo, Butch, Cassidy
-        
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("how many houses? >> ");
+        n = sc.nextInt();
+        System.out.println(n);
+
+        int[][] = new int[n][n];
+        arr = 0;
+        arr[1] = 6
+        arr[2] = 12;
+        arr[3] = 5;
+        arr[1] =
     }
 }
