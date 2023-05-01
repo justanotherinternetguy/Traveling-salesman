6ab208df456fa7583049223f5d9210da39c8164f
justanotherinternetguy <hendryxu1@gmail.com>
2023-05-01 12:10:11 -0400

    Commit

---
+++ Brute_Force.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Brute_Force {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>;
+        // Home, Aldo, Butch, Cassidy
+        
+    }
+}
